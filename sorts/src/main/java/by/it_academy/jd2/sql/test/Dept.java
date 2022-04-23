111a63b3cf2d643864a6b4f39709343c62d81bc0
qwester91 <95618038+qwester91@users.noreply.github.com>
2022-04-23 21:45:19 +0300

    Delete sorts/src/main/java/by/it_academy/jd2/sql/test directory

--- sorts/src/main/java/by/it_academy/jd2/sql/test/Dept.java
+++
@@ -1,7 +0,0 @@
-package by.it_academy.jd2.sql.test;
-
-public class Dept {
-   private int deptNo;
-   private String dName;
-   private String loc;
-}
--- sorts/src/main/java/by/it_academy/jd2/sql/test/Emp.java
+++
@@ -1,9 +0,0 @@
-package by.it_academy.jd2.sql.test;
-
-public class Emp {
-     private int empNo;
-     private String eName;
-     private String job;
-     private Dept deptNo;
-
-}
